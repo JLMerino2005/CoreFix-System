d6d89c1527964653b641c0480273da5f02f67e52
Jose Luis Merino Mota <merinomotajoseluis@gmail.com>
2026-04-11 00:02:54 -0600

    actualizacion de la web

--- sw.js
+++ sw.js
@@ -1,17 +1,21 @@
 const CACHE_NAME = 'corefix-v1';
+// Quitamos las diagonales iniciales y corregimos el nombre del Logo
 const assets = [
-  '/',
-  '/index.html',
-  '/style.css',
-  '/script.js',
-  '/img/logo.jpeg'
+  './',
+  'index.html',
+  'style.css',
+  'script.js',
+  'img/Logo.png', // Corregido: L mayúscula y extensión .png
+  'cotizar.html',
+  'rastreo.html'
 ];
 
 self.addEventListener('install', e => {
   e.waitUntil(
     caches.open(CACHE_NAME).then(cache => {
+      console.log('Cacheando archivos de CoreFix...');
       return cache.addAll(assets);
-    })
+    }).catch(err => console.log('Error de cacheo:', err))
   );
 });
 
